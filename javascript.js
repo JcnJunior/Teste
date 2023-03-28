fa0ff6ba00bd419ddd71d61c7980e98a1749c02c
JuniorNascimento <jcn.jr@outlook.com>
2023-03-28 18:17:14 -0300

    fff

--- javascript.js
+++ javascript.js
@@ -1,12 +1,12 @@
-const dataAtual = new Date(2023, 03 - 1, 28, 00, 00, 0);
-const dataFinal = new Date(2023, 03 - 1, 28, 00, 13, 0);
-const amanha = new Date(2023, 03 - 1, 29, 00 - 3, 00 - 13, 0);
+// const dataAtual = new Date(2023, 03 - 1, 28, 00, 00, 0);
+// const dataFinal = new Date(2023, 03 - 1, 28, 00, 13, 0);
+// const amanha = new Date(2023, 03 - 1, 29, 00 - 3, 00 - 13, 0);
 
-function criarAgendas() {
-  while (dataFinal <= amanha) {
-    dataAtual == dataAtual.setMinutes(dataAtual.getMinutes() + 20);
-    dataFinal == dataFinal.setMinutes(dataFinal.getMinutes() + 20);
-    console.log(JSON.stringify(dataFinal))
-  }
-}
-criarAgendas();
+// function criarAgendas() {
+//   while (dataFinal <= amanha) {
+//     dataAtual == dataAtual.setMinutes(dataAtual.getMinutes() + 20);
+//     dataFinal == dataFinal.setMinutes(dataFinal.getMinutes() + 20);
+//     console.log(JSON.stringify(dataFinal))
+//   }
+// }
+// criarAgendas();
